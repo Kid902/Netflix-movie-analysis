ca1daa436a184e64715db85ee62f2c76d81bc200
Ayush kumar <mikkikumar374@gmail.com>
2025-05-13 20:38:03 +0530

    Add files via upload

---
+++ Data preprocessing.py
@@ -0,0 +1,4 @@
+# • we have a dataframe consisting of 9827 rows and 9 columns.
+# • our dataset looks a bit tidy with no NaNs nor duplicated values.
+# • Release_Date column needs to be casted into date time and to extract only the
+# • Overview, Original_Languege and Poster-Url wouldn't be so useful during analys
