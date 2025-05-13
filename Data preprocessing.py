88120a97c3b6d4af0e1f8ea0415c84e931592176
Ayush kumar <mikkikumar374@gmail.com>
2025-05-13 20:46:01 +0530

    Delete Data preprocessing.py

--- Data preprocessing.py
+++
@@ -1,4 +0,0 @@
-# • we have a dataframe consisting of 9827 rows and 9 columns.
-# • our dataset looks a bit tidy with no NaNs nor duplicated values.
-# • Release_Date column needs to be casted into date time and to extract only the
-# • Overview, Original_Languege and Poster-Url wouldn't be so useful during analys
