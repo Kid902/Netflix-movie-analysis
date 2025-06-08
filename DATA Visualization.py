e8e067de2c76e69792b09c02c32b0cc281ea9c9c
Ayush kumar <mikkikumar374@gmail.com>
2025-06-08 10:55:52 +0530

    Update DATA Visualization.py

--- DATA Visualization.py
+++ DATA Visualization.py
@@ -45,21 +45,4 @@ plt.show()
 
 
 
-#  What is the most frequent genre in the dataset?
-# Drama genre is the most frequent genre in our dataset and has appeared more than
-# 14% of the times among 19 other genres.
 
-#  What genres has highest votes ?
-# we have 25.5% of our dataset with popular vote (6520 rows). Drama again gets the
-# highest popularity among fans by being having more than 18.5% of movies popularities.
-
-#  What movie got the highest popularity ? what's its genre ?
-# Spider-Man: No Way Home has the highest popularity rate in our dataset and it has
-# genres of Action , Adventure and Sience Fiction .
-
-#  What movie got the lowest popularity ? what's its genre ?
-# The united states, thread' has the highest lowest rate in our dataset
-# and it has genres of music , drama , 'war', 'sci-fi' and history`.
-
-#  Which year has the most filmmed movies?
-# year 2020 has the highest filmming rate in our dataset.
